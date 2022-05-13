8131e1db90588c70a9a6c45855d7afc73599e647
loominex <loominex@gmail.com>
2022-05-13 10:17:47 +0430

    Add Batch processing multi thread

--- MontyHall.Game/MontyGame.cs
+++ MontyHall.Game/MontyGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,74 +10,81 @@ namespace MontyHall.Game
     public class MontyGame
     {
         private const int doorCount = 3;
-        public GameResult Run(decimal simulationNo,bool changeDoor)
+        private object _locker = new object();
+        public GameResult Run(decimal simulationNo, bool changeDoor)
         {
             if (simulationNo == 0)
             {
                 new GameResult();
             }
-            decimal trueGuess=0, wrongGuess = 0;
+            decimal trueGuess = 0, wrongGuess = 0;
 
-            var random=new Random();
+            var random = new Random();
 
-
-            for (int i = 1; i <= simulationNo; i++)
+            Parallel.ForEach(Partitioner.Create(0, (int)simulationNo), range =>
             {
-                #region select a door
-                //each time a random door is selected.
-                var selected = random.Next(1, doorCount + 1);
-
-                #endregion
-
-                var openedDoor = selected;
-
-                #region set prize door randomly
-
-                var prizeDoor = random.Next(1, doorCount + 1);
-
-                #endregion
-                //monty opens a door that has not prize behind and it's not selected.
-                #region opening a goat door
-
-                //here monty wants to open a goat door, but one that is not prize or selected, for example prize and selected
-                // doors are door number 1, monty can open either 2 or 3, he chooses first one.
-                openedDoor = Enumerable.Range(1, doorCount).First(x => x != prizeDoor && x != selected);
-
-                #endregion
-                
-                #region switch the door
-
-                if (changeDoor)
+                for (int i = range.Item1; i < range.Item2; i++)
                 {
-                    var newSelected = random.Next(1, doorCount + 1);
+                    #region select a door
+                    //each time a random door is selected.
+                    var selected = random.Next(1, doorCount + 1);
 
-                    //switch the door to closed door.
-                    //randomly chooses a door that is not opened or selected.
-                    while (newSelected==selected || newSelected==openedDoor)
+                    #endregion
+
+                    var openedDoor = selected;
+
+                    #region set prize door randomly
+
+                    var prizeDoor = random.Next(1, doorCount + 1);
+
+                    #endregion
+                    //monty opens a door that has not prize behind and it's not selected.
+                    #region opening a goat door
+
+                    //here monty wants to open a goat door, but one that is not prize or selected, for example prize and selected
+                    // doors are door number 1, monty can open either 2 or 3, he chooses first one.
+                    openedDoor = Enumerable.Range(1, doorCount).First(x => x != prizeDoor && x != selected);
+
+                    #endregion
+
+                    #region switch the door
+
+                    if (changeDoor)
                     {
-                          newSelected = random.Next(1, doorCount + 1);
+                        var newSelected = random.Next(1, doorCount + 1);
+
+                        //switch the door to closed door.
+                        //randomly chooses a door that is not opened or selected.
+                        while (newSelected == selected || newSelected == openedDoor)
+                        {
+                            newSelected = random.Next(1, doorCount + 1);
+                        }
+                        selected = newSelected;
                     }
-                    selected= newSelected;
+
+                    #endregion
+
+                    #region set guess result
+
+                    lock (_locker) 
+                    {
+                        if (prizeDoor == selected)
+                        {
+                            trueGuess++;
+                        }
+                        else
+                        {
+                            wrongGuess++;
+                        }
+                    }
+                    #endregion
+
                 }
-
-                #endregion
-
-                #region set guess result
-
-                if (prizeDoor == selected)
-                {
-                    trueGuess++;
-                }
-                else
-                {
-                    wrongGuess++;
-                }
-
-                #endregion
-
-            }
+            });
+            
 
             return new GameResult() { TrueGuess=trueGuess,WrongGuess=wrongGuess};
         }
+
     }
 }
